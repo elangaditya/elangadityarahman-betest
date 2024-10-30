8ceea7ea2e9008446576fc5278197b9b2872cfcd
Elang Aditya Rahman <elangadityar@gmail.com>
2024-10-30 14:09:53 +0700

    implement jwt auth

---
+++ src/middleware/jwt.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { NextFunction, Request, Response } from "express";
+
+dotenv.config();
+
+export function generateAccessToken() {
+  const payload: any = {
+    date: new Date(),
+  };
+  return jwt.sign(payload, process.env.TOKEN_SECRET as string, { expiresIn: 600 });
+}
+
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(403);
+
+    next();
+  });
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import auth from "./auth.route";
+import { authenticateToken, generateAccessToken } from "../middleware";
 
 const router = express.Router();
 
-router.use("/api/users", auth);
+router.use("/api/users", authenticateToken, auth);
+
+router.get("/api/jwt", (req: Request, res: Response) => {
+  const token = generateAccessToken();
+
+  return res.status(200).send({ err: false, token });
+});
 
 export default router;
